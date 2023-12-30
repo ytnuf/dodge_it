54e2f3ca888a7c76d69e91265151dcd73f364ac4
ytnuf <161308826+ytnuf@users.noreply.github.com>
2023-12-30 23:22:45 +0000

    Player follows mouse.

--- player/player.gd
+++ player/player.gd
@@ -1,3 +1,12 @@
 
 class_name Player
 extends Area2D
+
+
+func _physics_process(_dt: float) -> void:
+	position = get_global_mouse_position()
+
+	const WIN_SIZE := Vector2(1152.0, 640.0)
+	const RADIUS := 16.0
+	position.x = clamp(RADIUS, position.x, WIN_SIZE.x-RADIUS)
+	position.y = clamp(RADIUS, position.y, WIN_SIZE.y-RADIUS)
