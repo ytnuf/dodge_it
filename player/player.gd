39aadebd7f05b83eb320230cdd7fba5d844ddc3d
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-01-03 17:27:56 +0000

    Changed resolution size.

--- player/player.gd
+++ player/player.gd
@@ -6,7 +6,8 @@ extends Area2D
 func _physics_process(_dt: float) -> void:
 	position = get_global_mouse_position()
 
-	const WIN_SIZE := Vector2(1152.0, 640.0)
+	var win_w : float = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var win_h : float = ProjectSettings.get_setting("display/window/size/viewport_height")
 	const RADIUS := 16.0
-	position.x = clamp(RADIUS, position.x, WIN_SIZE.x-RADIUS)
-	position.y = clamp(RADIUS, position.y, WIN_SIZE.y-RADIUS)
+	position.x = clamp(RADIUS, position.x, win_w - RADIUS)
+	position.y = clamp(RADIUS, position.y, win_h - RADIUS)
