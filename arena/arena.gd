1089c2d087017dc58d07eb3f2764e462e878d38e
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-01-08 00:45:22 +0000

    Changed arena size.

--- arena/arena.gd
+++ arena/arena.gd
@@ -2,22 +2,30 @@
 extends Node2D
 
 
-@onready var _bottomright_spawnpoint : Vector2 = $BottomRightSpawnMarker.position
+@onready var _bottom_right : Vector2 = $BottomRight.position
 @onready var _enemies := $Enemies
 @onready var _player : Player = $Player
-@onready var _topleft_spawnpoint : Vector2 = $TopLeftSpawnMarker.position
+@onready var _top_left : Vector2 = $TopLeft.position
+
+
+func _ready() -> void:
+	_player.arena_bottom = _bottom_right.y
+	_player.arena_left = _top_left.x
+	_player.arena_right = _bottom_right.x
+	_player.arena_top = _top_left.y
 
 
 func _on_timer_timeout() -> void:
 	const ENEMY_SCENE := preload("res://enemies/enemy.tscn")
 	var enemy : Enemy = ENEMY_SCENE.instantiate()
 
-	var MIN_SQ_DIST_FROM_PLAYER := 128.0 * 128.0
+	const MIN_SQ_DIST_FROM_PLAYER := 128.0 * 128.0
+	const MAX_ENEMY_SIZE := 64.0
 	var pos := _player.position
 	while pos.distance_squared_to(_player.position) <= MIN_SQ_DIST_FROM_PLAYER:
 		pos = Vector2(
-			randf_range(_topleft_spawnpoint.x, _bottomright_spawnpoint.x),
-			randf_range(_topleft_spawnpoint.y, _bottomright_spawnpoint.y),
+			randf_range(_top_left.x + MAX_ENEMY_SIZE, _bottom_right.x - MAX_ENEMY_SIZE),
+			randf_range(_top_left.y + MAX_ENEMY_SIZE, _bottom_right.y - MAX_ENEMY_SIZE),
 		)
 	enemy.position = pos
 
--- player/player.gd
+++ player/player.gd
@@ -2,17 +2,21 @@
 class_name Player
 extends Area2D
 
+
 signal died
+
+var arena_bottom : float
+var arena_left : float
+var arena_right : float
+var arena_top : float
+@onready var _radius : float = $CollisionShape2D.shape.radius
 
 
 func _physics_process(_dt: float) -> void:
 	position = get_global_mouse_position()
 
-	var win_w : float = ProjectSettings.get_setting("display/window/size/viewport_width")
-	var win_h : float = ProjectSettings.get_setting("display/window/size/viewport_height")
-	const RADIUS := 16.0
-	position.x = clamp(RADIUS, position.x, win_w - RADIUS)
-	position.y = clamp(RADIUS, position.y, win_h - RADIUS)
+	position.x = clamp(arena_left + _radius, position.x, arena_right - _radius)
+	position.y = clamp(arena_top + _radius, position.y, arena_bottom - _radius)
 
 
 func _on_hit() -> void:
