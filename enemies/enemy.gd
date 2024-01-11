3727845e21e006dce8251e0882cbe141f121491c
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-01-11 20:25:10 +0000

    Removed redundant code in enemy.gd.

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -4,13 +4,6 @@ extends RigidBody2D
 
 
 @export var init_vel := Vector2(32, 32)
-@export var rel_size := 1.0
-
-
-func _ready() -> void:
-	mass = rel_size
-	$CollisionShape2D.shape.radius *= rel_size
-	$Polygon2D.scale *= rel_size
 
 
 func set_radius(radius: float) -> void:
