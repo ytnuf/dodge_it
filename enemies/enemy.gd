5ac1b1e2b243a03fcbdbb3b549a07797d44d9a59
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-01-13 16:09:34 +0000

    Added sound effects for collisions.

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 
 
 @export var init_vel := Vector2(32, 32)
+@onready var _billiard_collide := $BilliardCollideSfx
+@onready var _wall_hit := $WallHitSfx
 
 
 func set_radius(radius: float) -> void:
@@ -12,4 +14,13 @@ func set_radius(radius: float) -> void:
 	assert(is_equal_approx(default_radius * 2.0, $Sprite2D.texture.get_height() ) )
 	assert(default_radius != 0.0)
 	$Sprite2D.scale *= radius / default_radius
+
+
+
+func _on_body_entered(node: Node) -> void:
+	var body := node as PhysicsBody2D
+	if body.collision_layer == 2:
+		_billiard_collide.play()
+	elif body.collision_layer == 4:
+		_wall_hit.play()
 
