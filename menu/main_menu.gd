1b1e60b2895567d6d8daf09e6e73b765173ee79e
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-01-16 01:55:41 +0000

    Created main menu scene.

---
+++ menu/main_menu.gd
@@ -0,0 +1,10 @@
+
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://arena/arena.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
