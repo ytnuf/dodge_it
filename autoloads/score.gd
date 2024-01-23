bb98b22f4a7ebfbc6a76b4055270dba23f6f8b4c
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-01-23 01:41:36 +0000

    High Score now saves.

--- autoloads/score.gd
+++ autoloads/score.gd
@@ -1,5 +1,17 @@
 
 extends Node
 
+
+const SAVE_FILE_PATH := &"user://score.save"
+
 var latest := 0
-var best := 0
+var best := 0:
+	set(val):
+		best = val
+		var save_file := FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+		save_file.store_64(best)
+
+func _ready() -> void:
+	if FileAccess.file_exists(SAVE_FILE_PATH):
+		var save_file := FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+		best = save_file.get_64()
