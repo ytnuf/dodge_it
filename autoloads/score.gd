9d9ec1a9520a1ae7ad8d664f18e49c74c1a35c7a
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-02-28 15:43:50 +0000

    Add versioning to saved score

--- autoloads/score.gd
+++ autoloads/score.gd
@@ -3,15 +3,24 @@ extends Node
 
 
 const SAVE_FILE_PATH := &"user://score.save"
+const VERSION := &"v1"
+
 
 var latest := 0
+
 var best := 0:
 	set(val):
 		best = val
-		var save_file := FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
-		save_file.store_64(best)
+		var cfg := ConfigFile.new()
+		cfg.set_value(VERSION, &"score", best)
+		cfg.save(SAVE_FILE_PATH)
+
 
 func _ready() -> void:
 	if FileAccess.file_exists(SAVE_FILE_PATH):
-		var save_file := FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
-		best = save_file.get_64()
+		var cfg := ConfigFile.new()
+		var err := cfg.load(SAVE_FILE_PATH)
+		if err:
+			print("Unable to load save file: ", err)
+			return
+		best = cfg.get_value(VERSION, &"score", 0)
