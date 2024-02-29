2bfce08deaf1ee5ac3e8f29099a3b02136c231b1
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-02-29 16:48:00 +0000

    Create credit scene

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -5,6 +5,9 @@ extends Control
 func _ready() -> void:
 	$VBoxContainer/ScoreLabel.text = "High Score: %d" % Score.best
 
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu/credits.tscn")
+
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://arena/arena.tscn")
