805621ce71d1d946a65451d33255c8b9c9cd2998
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-02-29 12:08:57 +0000

    Display high score to main menu

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -2,6 +2,10 @@
 extends Control
 
 
+func _ready() -> void:
+	$VBoxContainer/ScoreLabel.text = "High Score: %d" % Score.best
+
+
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://arena/arena.tscn")
 
